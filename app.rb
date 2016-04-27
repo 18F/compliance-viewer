28e81f74736046225a3c771dcba53c23adc561d7
Julia Elman <julia.elman@gsa.gov>
2016-04-27 15:11:31 -0400

    Removing logout from main nav.

--- app.rb
+++ app.rb
@@ -49,11 +49,6 @@ class ComplianceViewer < Sinatra::Base
     redirect '/'
   end
 
-  get '/auth/logout' do
-    session[:user_email] = nil 
-    redirect "https://alpha.my.usa.gov/users/sign_out?continue=http://#{host_with_port}/"
-  end
-
   before %r{^(?!\/auth)} do
     redirect '/auth/myusa' unless authed?
   end
