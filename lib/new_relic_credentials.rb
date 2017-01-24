a995dac2c33bcd093431bfd6523658f00508d8e0
James C. Scott <jamesc.scott@gsa.gov>
2017-01-24 12:38:53 -0500

    Fix codeclimate errors

--- lib/new_relic_credentials.rb
+++ lib/new_relic_credentials.rb
@@ -2,6 +2,6 @@ require 'cfenv'
 
 class NewRelicCredentials
   def self.license_key
-      Cfenv.service('user-provided').credentials.new_relic_license_key
+    Cfenv.service('user-provided').credentials.new_relic_license_key
   end
 end
